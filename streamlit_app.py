d1112dac683a3fec66c125e415a7cc6197a15354
nevilledata <123775420+nevilledata@users.noreply.github.com>
2023-01-30 21:48:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ streamlit.dataframe(fruits_to_show)
 # New Secton to Display fruityvice api response
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
