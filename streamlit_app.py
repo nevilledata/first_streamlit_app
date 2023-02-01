8fd2e5033b5ba4d5b98168a55607f68ec3360bf1
nevilledata <123775420+nevilledata@users.noreply.github.com>
2023-02-01 22:23:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-#import pandas
-#import requests
+import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
