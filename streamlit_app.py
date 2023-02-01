34203481f0af44d6080d88c913bf0dee61d7ad90
nevilledata <123775420+nevilledata@users.noreply.github.com>
2023-02-01 22:36:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,6 +24,12 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+#Create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 # New Secton to Display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,9 +37,8 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_Choice)
+      streamlit.dataframe(back_from_function)
       
 except URLError as e:
     streamlit.error()
